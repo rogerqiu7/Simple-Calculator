35b709e23f10924ff577dfb56dfc0ce6f9c856f6
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2022-05-14 13:48:06 -0500

    update

--- script.js
+++ script.js
@@ -1,160 +1,147 @@
-// this section sets default values and basic functions of operator buttons 
-
-// create empty default current operands
-let firstOperand = ''
-let secondOperand = ''
-let currentOperation = null
-let shouldResetScreen = false
-
-// this section sets html elements and variables for the script
-// and which actions call which functions along with its arguement
-
-//set variables for all html elements
-// querySelectorAll selects all numbers and operators selects all operators
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatorButtons = document.querySelectorAll('[data-operator]')
-const equalsButton = document.getElementById('equalsBtn')
-const clearButton = document.getElementById('clearBtn')
-const deleteButton = document.getElementById('deleteBtn')
-const pointButton = document.getElementById('pointBtn')
-const lastOperationScreen = document.getElementById('lastOperationScreen')
-const currentOperationScreen = document.getElementById('currentOperationScreen')
-
-// set events for buttons onclick
-window.addEventListener('keydown', handleKeyboardInput)
-equalsButton.addEventListener('click', evaluate)
-clearButton.addEventListener('click', clear)
-deleteButton.addEventListener('click', deleteNumber)
-pointButton.addEventListener('click', appendPoint)
-
-// for each number button clicked, append that number using append number function
-numberButtons.forEach((button) =>
-  button.addEventListener('click', () => appendNumber(button.textContent))
-)
-
-// for each operator button clicked, use set operation function to call that operator
-operatorButtons.forEach((button) =>
-  button.addEventListener('click', () => setOperation(button.textContent))
-)
-
-//append number function, if sceen is 0 or reset screen is true, then reset the screen and append the number to current operation screen
-function appendNumber(number) {
-  if (currentOperationScreen.textContent === '0' || shouldResetScreen)
-    resetScreen()
-  currentOperationScreen.textContent += number
-}
-
-//sets current operation screen to blank
-function resetScreen() {
-  currentOperationScreen.textContent = ''
-  shouldResetScreen = false
-}
-
-// clear function sets all to 0 and blank
-function clear() {
-  currentOperationScreen.textContent = '0'
-  lastOperationScreen.textContent = ''
-  firstOperand = ''
-  secondOperand = ''
-  currentOperation = null
-}
-
-// append point appends . to text content
-function appendPoint() {
-  if (shouldResetScreen) resetScreen()
-  if (currentOperationScreen.textContent === '')
-    currentOperationScreen.textContent = '0'
-  if (currentOperationScreen.textContent.includes('.')) return
-  currentOperationScreen.textContent += '.'
-}
-
-// delete one string from current operations
-function deleteNumber() {
-  currentOperationScreen.textContent = currentOperationScreen.textContent
-    .toString()
-    .slice(0, -1)
-}
-
-// use operator on text context if current operation is not null
-function setOperation(operator) {
-  if (currentOperation !== null) evaluate()
-  firstOperand = currentOperationScreen.textContent
-  currentOperation = operator
-  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
-  shouldResetScreen = true
-}
-
-// if current operation isnt null or reset screen or divide by zero, perform operation on first and second operand.
-function evaluate() {
-  if (currentOperation === null || shouldResetScreen) return
-  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
-    alert("You can't divide by 0!")
-    return
+  // select all button elements as variables
+  // select previous and current operands as variables
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operation]')
+  const equalsButton = document.querySelector('[data-equals]')
+  const deleteButton = document.querySelector('[data-delete]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  
+// create class, constructor takes previous and current text elements
+//set the this. variables
+// clear calculator every time new one is opened
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+    }
+  
+    // set clear function, remove all values on screen
+    clear() {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
+    }
+  
+    // set delete function, set current operand to string, delete last value
+    // select all numbers from index 0 to one from end
+    delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+  
+    // set append function that adds number to screen
+    // if number is period, and current operand already has period, just return
+    // set current operand to appending number as a string to it
+    appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return
+      this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+  
+    // when typing a number then clicking an operation button, bring both number and operation back to previous operand 
+    // check if there are numbers first for us to perform on
+    // another check, if previous operand is not empty string, do the computation
+    // set operation = operation we passed in
+    // were done typing the current number so we recycle it to previous and clear out current
+    chooseOperation(operation) {
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') {
+        this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
+    }
+  
+    // take values in calculator and compute a single value
+    // convert prevoius and current string to numbers
+    // check if there is values, return if empty
+    // if statements chained, if plus, minus, multiply, divide: execute computation to previous plus current. else return 
+    compute() {
+      let computation
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+        case '+':
+          computation = prev + current
+          break
+        case '-':
+          computation = prev - current
+          break
+        case '*':
+          computation = prev * current
+          break
+        case '÷':
+          computation = prev / current
+          break
+        default:
+          return
+      }
+      
+      // set current operand as computation
+      // clear previous and operation 
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
+    }
+  
+    //update the values inside the output 
+    getDisplayNumber(number) {
+      return number
+     }
+  
+    //  set previous operand and text element as prevoius operand
+    // if operation isnt null, add the operation appended to number
+    updateDisplay() {
+      this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+      if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+        this.previousOperandTextElement.innerText = ''
+      }
+    }
   }
-  secondOperand = currentOperationScreen.textContent
-  currentOperationScreen.textContent = roundResult(
-    operate(currentOperation, firstOperand, secondOperand)
-  )
-  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
-  currentOperation = null
-}
-
-// rounds the number to 3 decimal places
-function roundResult(number) {
-  return Math.round(number * 1000) / 1000
-}
-
-// set keyboard inputs for calculator
-function handleKeyboardInput(e) {
-  if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
-  if (e.key === '.') appendPoint()
-  if (e.key === '=' || e.key === 'Enter') evaluate()
-  if (e.key === 'Backspace') deleteNumber()
-  if (e.key === 'Escape') clear()
-  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-    setOperation(convertOperator(e.key))
-}
-
-// set keyboard operator inputs for calculator
-function convertOperator(keyboardOperator) {
-  if (keyboardOperator === '/') return '÷'
-  if (keyboardOperator === '*') return '×'
-  if (keyboardOperator === '-') return '−'
-  if (keyboardOperator === '+') return '+'
-}
-
-//create operator functions
-function add(a, b) {
-  return a + b
-}
-
-function substract(a, b) {
-  return a - b
-}
-
-function multiply(a, b) {
-  return a * b
-}
-
-function divide(a, b) {
-  return a / b
-}
-
-// create function to perform operations on a and b
-function operate(operator, a, b) {
-  a = Number(a)
-  b = Number(b)
-  switch (operator) {
-    case '+':
-      return add(a, b)
-    case '−':
-      return substract(a, b)
-    case '×':
-      return multiply(a, b)
-    case '÷':
-      if (b === 0) return null
-      else return divide(a, b)
-    default:
-      return null
-  }
-}
+  
+  // define class that takes constructors from previous class
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+  // for all number buttons, add event listener for when clicked on
+  // take calculator and add number of whatever is in button
+  // update the display right after
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+  // for all operations buttons, when clicked, choose operation takes button 
+  // update the display
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+  // equals button, onclick, computes and displays it
+  equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+  })
+  
+  // all clear button, onclick, performs clear function
+  allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+  })
+  
+  // delete button, onclick, performs delete function
+  deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
